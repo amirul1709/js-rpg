4a1bc8ef06b1c17bedf854290ecab1baa3b6bf80
Amirul Hossain <amirul1709@hotmail.com>
2025-03-14 16:32:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,20 +17,43 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button function": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"store\".",
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button function": [buyHealth, buyWeapon, goTown],
+        text: "Go to town square.",
+    }
+];
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function goStore() {
-    text.innerText = "You enter the store";
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
+function update(location) {
+    text.innerText = location[text];
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
 
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
+    button1.onclick = location["button function"][0];
+    button2.onclick = location["button function"][1];
+    button3.onclick = location["button function"][2];
+}
+
+function goTown() {
+    update(locations[0]);
+}
+
+function goStore() {
+    update(locations[1]);
 }
 
 function goCave() {
@@ -40,3 +63,12 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon")
 }
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
