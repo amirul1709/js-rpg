ff44b4c9d7aaba00e4499c3b77d74293e2bc2fb2
Amirul Hossain <amirul1709@hotmail.com>
2025-03-15 12:07:08 -0600

    Implemented buying weapons

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+    {
+        name: "stick",
+        power: 5
+    },
+    {
+        name: "dagger",
+        power: 30
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100
+    }
+];
+
 const locations = [
     {
         name: "town square",
@@ -64,15 +83,35 @@ function goStore() {
 
 function buyHealth() {
     if (gold >= 10) {
-        gold -= 10
-        health += 10
+        gold -= 10;
+        health += 10;
         goldText.innerText = gold;
         healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health."
     }
 }
 
 function buyWeapon() {
-    gold -= 30;
+
+    if (currentWeapon < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon += 1;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += "In your inventory you have: " + inventory + ".";
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+
 }
 
 function goCave() {
